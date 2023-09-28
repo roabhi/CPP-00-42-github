8c93ad9add6c629017289d02eeed803cdbb6b0f0
Roberto Abril hidalgo <rabril-h@car3s4.42barcelona.com>
2023-09-28 21:27:24 +0200

    uptodate

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,11 +6,7 @@
 /*   By: rabril-h <rabril-h@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/25 18:58:46 by rabril-h          #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2023/09/27 20:37:11 by rabril-h         ###   ########.fr       */
-=======
-/*   Updated: 2023/09/28 18:47:58 by rabril-h         ###   ########.fr       */
->>>>>>> c8776f216c8a77b5f44ead80ea2efb587b4152c6
+/*   Updated: 2023/09/28 21:26:46 by rabril-h         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,31 +17,19 @@ int main(int argc, char *argv[])
     std::string input;
     int         i;
     Phonebook   phonebook;
-<<<<<<< HEAD
-    Contact     contact;
-=======
     // Contact     contact;
->>>>>>> c8776f216c8a77b5f44ead80ea2efb587b4152c6
 
     input = " ";
     i = -1;
     (void)argv;
-<<<<<<< HEAD
-=======
     
->>>>>>> c8776f216c8a77b5f44ead80ea2efb587b4152c6
 
     if(argc != 1)
         return (0);
     std::cout << WELCOME_MSG << std::endl;
     std::cout << INFO_MSG << std::endl;
-<<<<<<< HEAD
-    while (1)
-    {
-=======
     while (1)    {
         
->>>>>>> c8776f216c8a77b5f44ead80ea2efb587b4152c6
         std::cout << "-> ";
         if (!std::getline(std::cin, input))
 			return (1);
@@ -54,17 +38,6 @@ int main(int argc, char *argv[])
         i = -1;
         if (input == "EXIT")
             return (0);
-<<<<<<< HEAD
-        else if (input == "ADD") 
-            phonebook.addContact(contact);
-        else if (input == "SEARCH")
-             std::cout << "search for a contact here" << std::endl;
-        else
-        {
-            std::cout << ERROR_MSG << std::endl; 
-            std::cout << INFO_MSG << std::endl;        
-        }
-=======
         else if (input == "ADD")
             phonebook.addContact();
         else if (input == "SEARCH")
@@ -75,7 +48,6 @@ int main(int argc, char *argv[])
         //     std::cout << ERROR_MSG << std::endl; 
         //     std::cout << INFO_MSG << std::endl;        
         // }
->>>>>>> c8776f216c8a77b5f44ead80ea2efb587b4152c6
     }   
     return (0);
 }
